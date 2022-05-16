467f6cb0b60aa7a9c0dcf23c87bfa74c19364cbb
ST10106902 <101652191+ST10106902@users.noreply.github.com>
2022-05-16 20:44:26 +0200

    Add files via upload

---
+++ JavaApplication4Test.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+
+import javaapplication4.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author COMPUTER
+ */
+public class JavaApplication4Test {
+    
+    public void JavaApplication4Test() {
+    }
+
+       
+                
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tear
+        () {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
